e44145603189439d2777bc8b33c4697829c61760
Diego-Paris <45906185+Diego-Paris@users.noreply.github.com>
2020-01-05 02:44:57 -0600

    Screen now adjusts to monitor resolution

--- BrotherAsteroid/src/main/Launch.java
+++ BrotherAsteroid/src/main/Launch.java
@@ -1,10 +1,25 @@
 package main;
 
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+
 
 public class Launch {
 
     public static void main(String[] args) {
-        GameSetUp game = new GameSetUp("Brother Asteroid", 600, 600);
+    	
+    
+    	
+    	Rectangle screenSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+    	int screenHeight = screenSize.height / 2 +400;
+    	int screenWidth  = screenSize.width  / 2 +200;
+    	
+    	
+    	
+    	
+    	
+    	
+        GameSetUp game = new GameSetUp("Brother Asteroid", screenWidth, screenHeight);
         game.start();
     }
 }
